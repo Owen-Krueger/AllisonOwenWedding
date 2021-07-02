7f2776b087f3227b80fee882a1f78d6b7d66a80a
Owen Krueger <github@owenkrueger.dev>
2021-07-01 22:15:27 -0500

    Add styling for active link

---
+++ src/Shared/MainLayout.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace AllisonOwenWedding.Shared
+{
+    /// <summary>
+    /// The main layout.
+    /// </summary>
+    public partial class MainLayout
+    {
+        [Inject]
+        NavigationManager NavigationManager { get; set; }
+
+        /// <summary>
+        /// Get the class for a link based on the link.
+        /// </summary>
+        public string GetLinkClass(string link)
+        {
+            string relativeUrl = NavigationManager.ToBaseRelativePath(NavigationManager.Uri);
+            return relativeUrl.Equals(link) ? "active-link" : string.Empty;
+        }
+    }
+}
