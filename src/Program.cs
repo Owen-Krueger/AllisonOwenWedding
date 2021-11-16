bb6c97b280063c234ef853fa5c94efdfc794f782
Owen Krueger <github@owenkrueger.dev>
2021-11-15 18:15:10 -0600

    Switch over to simplified Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -1,20 +1,61 @@
-using Microsoft.AspNetCore.Hosting;
+using AllisonOwenWedding.DataAccess;
+using AllisonOwenWedding.Models;
+using AllisonOwenWedding.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Net;
+using System.Net.Mail;
 
-namespace AllisonOwenWedding
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton(new EmailVariables()
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    EmailUsername = builder.Configuration["EmailUsername"],
+    EmailRecipient = builder.Configuration["EmailRecipient"]
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+builder.Services.AddScoped(x =>
+{
+    return new SmtpClient()
+    {
+        Host = builder.Configuration["EmailHost"],
+        Port = Convert.ToInt32(builder.Configuration["EmailPort"]),
+        Credentials = new NetworkCredential(builder.Configuration["EmailUsername"], builder.Configuration["EmailPassword"]),
+        EnableSsl = true,
+        UseDefaultCredentials = false,
+        DeliveryMethod = SmtpDeliveryMethod.Network
+    };
 }
+);
+
+builder.Services.AddTransient<IEmailService, EmailService>();
+builder.Services.AddDbContext<IWeddingEntities, WeddingEntities>(options => options.UseSqlite(@builder.Configuration["DataSource"]));
+builder.Services.AddTransient<IWeddingService, WeddingService>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapBlazorHub();
+    endpoints.MapFallbackToPage("/_Host");
+});
+
+app.Run();
